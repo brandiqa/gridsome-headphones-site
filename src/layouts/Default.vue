bbe55f76057b7039ab8e4068d6ae83ff0d6ced3e
Michael Wanyoike <micwanyoike@gmail.com>
2020-10-13 14:49:33 +0300

    minor tailwind sorting

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="container mx-auto">
-    <header class="flex justify-between py-8">
+    <header class="flex justify-between my-4">
       <strong>
         <g-link to="/">{{ $static.metadata.siteName }}</g-link>
       </strong>
       <nav class="">
-        <g-link class="mr-4 text-green-700 font-semibold hover:underline hover:text-green-500 active:text-green-400" to="/"
+        <g-link
+          class="mr-4 font-semibold text-green-700 hover:underline hover:text-green-500 active:text-green-400"
+          to="/"
           >Home</g-link
         >
-        <g-link class="mr-4 text-green-700 font-semibold hover:underline hover:text-green-500 active:text-green-400" to="/about/">About</g-link>
+        <g-link
+          class="mr-4 font-semibold text-green-700 hover:underline hover:text-green-500 active:text-green-400"
+          to="/about/"
+          >About</g-link
+        >
       </nav>
     </header>
     <slot />
@@ -25,22 +31,10 @@ query {
 
 <style>
 body {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
-/* body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
+/*
 .header {
   display: flex;
   justify-content: space-between;
@@ -48,8 +42,5 @@ body {
   margin-bottom: 20px;
   height: 80px;
 }
-
-.nav__link {
-  margin-left: 20px;
-} */
+*/
 </style>
