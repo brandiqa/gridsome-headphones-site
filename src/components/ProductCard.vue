fe413e1c6f49dc7a980e6a4a11e3199ebe440109
Michael Wanyoike <micwanyoike@gmail.com>
2020-10-16 12:11:45 +0300

    Refactored: Created Product card

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="mb-4 overflow-hidden bg-white rounded shadow-lg">
+    <img
+      class="p-8"
+      v-bind:src="
+        edge.node.content.image | resize('fit-in/300x300/filters:fill(fff)')
+      "
+    />
+    <div class="flex justify-between px-8">
+      <g-link
+        class="block text-lg font-bold hover:underline hover:text-gray-700"
+        :to="edge.node.full_slug"
+      >
+        {{ edge.node.content.brand }} {{ edge.node.content.model }}
+      </g-link>
+      <span class="text-lg font-semibold text-green-700"
+        >${{ edge.node.content.price }}</span
+      >
+    </div>
+    <div
+      class="flex justify-between p-4 mt-3 text-sm text-gray-600 border-t-2 border-gray-400"
+    >
+      <span>{{ edge.node.content.driver | driverType }}</span>
+      <span>{{ edge.node.content.backType | backType }}</span>
+      <span>{{ edge.node.content.impedance }} Ohms</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['edge'],
+}
+</script>
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -4,35 +4,8 @@
       {{ $static.products.totalCount }} headphones available
     </p>
     <div class="flex flex-wrap mt-8 space-x-4">
-      <div
-        class="mb-4 overflow-hidden bg-white rounded shadow-lg"
-        v-for="edge in $static.products.edges"
-        :key="edge.node.id"
-      >
-        <img
-          class="p-8"
-          v-bind:src="
-            edge.node.content.image | resize('fit-in/300x300/filters:fill(fff)')
-          "
-        />
-        <div class="flex justify-between px-8">
-          <g-link
-            class="block text-lg font-bold hover:underline hover:text-gray-700"
-            :to="edge.node.full_slug"
-          >
-            {{ edge.node.content.brand }} {{ edge.node.content.model }}
-          </g-link>
-          <span class="text-lg font-semibold text-green-700"
-            >${{ edge.node.content.price }}</span
-          >
-        </div>
-        <div
-          class="flex justify-between p-4 mt-3 text-sm text-gray-600 border-t-2 border-gray-400"
-        >
-          <span>{{ edge.node.content.driver | driverType }}</span>
-          <span>{{ edge.node.content.backType | backType }}</span>
-          <span>{{ edge.node.content.impedance }} Ohms</span>
-        </div>
+      <div v-for="edge in $static.products.edges" :key="edge.node.id">
+        <ProductCard :edge="edge" />
       </div>
     </div>
   </div>
@@ -52,3 +25,13 @@
     }
   }
 </static-query>
+
+<script>
+import ProductCard from './ProductCard'
+
+export default {
+  components: {
+    ProductCard,
+  },
+}
+</script>
