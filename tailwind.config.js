bc7a17c68dabd2d6ecb6ca8182d275030388cfc1
Michael Wanyoike <micwanyoike@gmail.com>
2020-10-16 18:29:56 +0300

    Fixed tailwind purging issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ module.exports = {
   },
   purge: {
     enabled: process.env.NODE_ENV === 'production',
-    content: ['components/*.vue', 'layouts/*.vue', 'pages/*.vue'],
+    content: [
+      'src/components/**/*.vue',
+      'src/layouts/**/*.vue',
+      'src/pages/**/*.vue',
+    ],
   },
   theme: {
     extend: {},
