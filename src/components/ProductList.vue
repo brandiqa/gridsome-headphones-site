3de259d3200d01c40072baca1fddf1d9189a0de0
Michael Wanyoike <micwanyoike@gmail.com>
2020-10-16 14:09:25 +0300

    Implemented sorting

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,10 +1,28 @@
 <template>
   <div class="container mx-auto mt-8">
-    <p class="text-sm font-semibold text-gray-600">
-      {{ $static.products.totalCount }} headphones available
-    </p>
+    <div class="flex items-center justify-between p-4 bg-gray-300">
+      <div class="inline-flex items-center space-x-4">
+        <label class="font-semibold text-gray-700 text-md" for="sort-input"
+          >Sort By</label
+        >
+        <select
+          class="px-2 py-2 text-sm leading-tight text-gray-700 bg-gray-100 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
+          id="sort-input"
+          v-model="sortBy"
+        >
+          <option value="default">Default</option>
+          <option value="name-asc">Name (A-Z)</option>
+          <option value="name-desc">Name (Z-A)</option>
+          <option value="price-lo">Price (Low to High)</option>
+          <option value="price-hi">Price (High to Low)</option>
+        </select>
+      </div>
+      <span class="text-sm font-semibold text-gray-600">
+        {{ $static.products.totalCount }} headphones available
+      </span>
+    </div>
     <div class="flex flex-wrap mt-8">
-      <div v-for="product in products" :key="product.id">
+      <div v-for="product in sortedProducts" :key="product.id">
         <ProductCard :product="product" />
       </div>
     </div>
@@ -38,17 +56,33 @@ export default {
   data() {
     return {
       products: null,
+      sortBy: 'default',
     }
   },
   created() {
-    // Extract content data into products
+    // Extract content & edge data into products
     this.products = this.$static.products.edges.map((edge) => {
       const product = edge.node.content
       product.full_slug = edge.node.full_slug
       product.name = edge.node.name
-      product.created_at = edge.node.created_at
+      product.created_at = new Date(edge.node.created_at)
       return product
     })
+  },
+  computed: {
+    sortedProducts() {
+      if (this.sortBy == 'name-asc') {
+        return this.products.sort((a, b) => a.name.localeCompare(b.name))
+      } else if (this.sortBy == 'name-desc') {
+        return this.products.sort((a, b) => b.name.localeCompare(a.name))
+      } else if (this.sortBy == 'price-lo') {
+        return this.products.sort((a, b) => a.price - b.price)
+      } else if (this.sortBy == 'price-hi') {
+        return this.products.sort((a, b) => b.price - a.price)
+      } else {
+        return this.products.sort((a, b) => b.created_at - a.created_at)
+      }
+    },
   },
 }
 </script>
